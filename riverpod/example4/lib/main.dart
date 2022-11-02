1a7aeda266bfea7fde31632f6068fbc0353d5fdd
Orket <developerorket@gmail.com>
2022-11-02 15:17:06 +0300

    stream providers

--- riverpod/example4/lib/main.dart
+++ riverpod/example4/lib/main.dart
@@ -17,15 +17,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+final names = [
+  'orket',
+  'sam',
+  'hozier',
+  'shanaya',
+  'mercy',
+  'shalom',
+];
+
+final counterStream = StreamProvider(((ref) {
+  return getCountStream();
+
+  //  Stream.periodic(
+  //     const Duration(seconds: 1), ((computationCount) => computationCount + 1));
+}));
+
+final namesStream = StreamProvider(((ref) =>
+    ref.watch(counterStream.stream).map((event) => names.getRange(0, event))));
+
+Stream<int> getCountStream() async* {
+  int count = 0;
+  while (count <= names.length) {
+    yield count++;
+    await Future.delayed(const Duration(seconds: 1));
+  }
+}
+
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var counterData = ref.watch(namesStream);
+
+    ref.listen(counterStream, ((previous, next) {
+      if (next.value == names.length) {
+        showDialog(
+            context: context,
+            builder: ((context) => AlertDialog(
+                  content: Text('End of list'),
+                )));
+      }
+    }));
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text('mi snippet'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            counterData.when(
+                data: (names) => Expanded(
+                      child: ListView.builder(
+                          itemCount: names.length,
+                          itemBuilder: ((context, index) => ListTile(
+                                title: Text(names.toList()[index]),
+                              ))),
+                    ),
+                error: (e, s) => const Text('Reached end of list'),
+                loading: () => const CircularProgressIndicator()),
+          ],
+        ),
       ),
     );
   }
