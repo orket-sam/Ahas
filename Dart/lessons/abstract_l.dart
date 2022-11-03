9c8df0ef4065e7a0ac958df4671c266f4d1cf804
Orket <developerorket@gmail.com>
2022-11-03 11:19:38 +0300

    abstract leson

--- Dart/lessons/abstract_l.dart
+++ Dart/lessons/abstract_l.dart
@@ -1,21 +1,48 @@
-abstract class Human {
-  final int age;
-  int adult = 18;
-  Human([this.age = 0]);
+abstract class Shape {
+  int dimension1, dimension2;
 
-  bool isAdult();
-}
+  Shape(this.dimension1, this.dimension2);
 
-class Orket extends Human {
-  Orket([int age = 0]) : super(age);
-  @override
-  bool isAdult() {
-    return age >= adult;
+  void area();
+  void perimeter(String name) {
+    print('The perimeter of this $name is ${2 * (dimension1 + dimension2)}');
   }
 }
 
-void main() {
-  Human sam = new Orket();
+class Square extends Shape {
+  Square(int length) : super(length, length);
+  @override
+  void area() {
+    print('The area of this square is ${dimension1 * dimension2}');
+  }
+}
 
-  print(sam.age);
+class Rectangle implements Shape {
+  Rectangle(this.dimension1, this.dimension2);
+  @override
+  void area() {
+    print(
+        'The perimeter of this rectangle is ${(2 * dimension1 + dimension2)}');
+  }
+
+  @override
+  void perimeter(String name) {
+    print('The area of this $name is ${dimension1 * dimension2}');
+  }
+
+  @override
+  int dimension1;
+
+  @override
+  int dimension2;
+}
+
+void main() {
+  var mySquare1 = Square(24);
+  var myRectangle = Rectangle(10, 20);
+
+  mySquare1.area();
+  mySquare1.perimeter('square');
+  myRectangle.area();
+  myRectangle.perimeter('Rectangle');
 }
