3ddedc9d150bbf0c33f856bab361a99c1da293e2
Orket <developerorket@gmail.com>
2022-11-11 13:17:09 +0300

    collartz refresh

--- Dart/collartz.dart
+++ Dart/collartz.dart
@@ -1,16 +1,16 @@
-void main() {
-  num no = 21;
-  print(no);
-  while (no != 1) {
-    no = collartz(no);
-    print(no);
+int collartz(int x) {
+  if (x % 2 == 0) {
+    x = x ~/ 2;
+  } else {
+    x = ((x + 1) ~/ 2);
   }
+  return x;
 }
 
-num collartz(num no) {
-  if (no % 2 == 0) {
-    return no / 2;
-  } else {
-    return 3 * no + 1;
+void main() {
+  int y = 1450;
+  while (y != 1) {
+    y = collartz(y);
+    print(y);
   }
 }
