d6d73fde302e879da906cfb84be2141eee1a5b18
Orket <developerorket@gmail.com>
2022-11-11 12:28:42 +0300

    insertion sort update

--- Dart/sorting_algs/insertion_sort.dart
+++ Dart/sorting_algs/insertion_sort.dart
@@ -1,16 +1,17 @@
-void main() {
-  insertionSort([1, 0, 2, 10, 101, 0]);
+void insertionSort(List<int> data) {
+  for (var i = 0; i < data.length; i++) {
+    for (var j = 0; j < data.length - i - 1; j++) {
+      if (data[j] > data[j + 1]) {
+        var temp = data[j];
+        data[j] = data[j + 1];
+        data[j + 1] = temp;
+      }
+      print(data);
+    }
+  }
 }
 
-void insertionSort(List<int> data) {
-  for (var i = 0; i <= data.length - 1; i++) {
-    for (var j = 0; j < data.length - 1 - i; j++) {
-      if (data[j] > data[j + 1]) {
-        var x = data[j];
-        data[j] = data[j + 1];
-        data[j + 1] = x;
-      }
-    }
-    print(data);
-  }
+void main() {
+  insertionSort(
+      [1000, 23, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6]);
 }
