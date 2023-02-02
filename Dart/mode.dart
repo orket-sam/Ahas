b542ad5f0899dc209f9122c85b39eabfacd71807
Orket <developerorket@gmail.com>
2023-02-02 22:35:47 +0300

    mode

---
+++ Dart/mode.dart
@@ -0,0 +1,15 @@
+void main() {
+  findMode([1, 2, 2, 3, 3]);
+}
+
+void findMode(List<int> numbers) {
+  var numberMap = {};
+  for (var element in numbers) {
+    if (numberMap[element] == null) {
+      numberMap[element] = 1;
+    } else {
+      numberMap[element] += 1;
+    }
+  }
+  print(numberMap);
+}
